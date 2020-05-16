9fe33c336eddc8f8756c4bed49ec73ac4e605bcf
Samuel Lancaster <samuellancaster66@gmail.com>
2020-05-15 20:08:13 -0600

    pushing changes to code

--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,8 @@ module.exports = function (app) {
 
     app.post("/api/notes", function (req, res) {
         var newNote = req.body;
-        if (notes === "") {
-            newNote.id = 0;
+        if (notes === []) {
+            newNote.id = 1;
         } else {
             newNote.id = notes.length - 1;
         }
@@ -16,20 +16,23 @@ module.exports = function (app) {
         var strNotes = JSON.stringify(notes);
         fs.writeFile("db/db.json", strNotes, function (err) {
             if (err) throw err;
-            console.log("note was sent");
+            console.log("Note was sent");
             return newNote;
         });
+        res.send("Updating notepage")
     });
 
     app.delete("/api/notes/:id", function (req, res) {
         var noteId = req.params.id;
         // splice(notes);
         for (i = 0; i < notes.length; i++) {
-            notes.splice(noteId[i]);
+            console.log(notes[i]);
+            notes.splice(noteId);
             var strNotes = JSON.stringify(notes);
             fs.writeFile("db/db.json", strNotes, function (err) {
                 if (err) throw err;
                 console.log("Note was removed");
+                res.send("Updating page with note removed");
             });
         }
     });
