4b98bdf61da2af6b4642d66c8c4fd3c4a0169464
Samuel Lancaster <samuellancaster66@gmail.com>
2020-05-15 21:17:00 -0600

    pushing changes to code

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
 
     app.post("/api/notes", function (req, res) {
         var newNote = req.body;
-        if (notes === []) {
+        if (notes === "") {
             newNote.id = 1;
         } else {
             newNote.id = notes.length - 1;
@@ -22,11 +22,23 @@ module.exports = function (app) {
         res.send("Updating notepage")
     });
 
+    // app.delete("/api/notes/:id", function (req, res) {
+    //     var noteId = req.params.id;
+    //     // splice(notes);
+    //     for (i = 0; i < notes.length; i++) {
+    //         console.log(notes[i]);
+    //         notes.splice(noteId);
+    //         var strNotes = JSON.stringify(notes);
+    //         fs.writeFile("db/db.json", strNotes, function (err) {
+    //             if (err) throw err;
+    //             console.log("Note was removed");
+    //             res.send("Updating page with note removed");
+    //         });
+    //     }
+    // });
+
     app.delete("/api/notes/:id", function (req, res) {
         var noteId = req.params.id;
-        // splice(notes);
-        for (i = 0; i < notes.length; i++) {
-            console.log(notes[i]);
             notes.splice(noteId);
             var strNotes = JSON.stringify(notes);
             fs.writeFile("db/db.json", strNotes, function (err) {
@@ -34,6 +46,5 @@ module.exports = function (app) {
                 console.log("Note was removed");
                 res.send("Updating page with note removed");
             });
-        }
     });
 }
